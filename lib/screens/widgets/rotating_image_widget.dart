3ba2a4e91831dbb4a61d4f42493a35c2322bcc2c
Sufiyan <ksufi7350@gmail.com>
2024-10-10 20:33:24 +0500

    resizing profie image for mobile layout

--- lib/screens/widgets/rotating_image_widget.dart
+++ lib/screens/widgets/rotating_image_widget.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-
 import 'dart:math' as math;
-
 import 'package:portfolio_web/constants/colors.dart';
 
 class RotatingImageContainer extends StatefulWidget {
@@ -13,23 +11,34 @@ class RotatingImageContainer extends StatefulWidget {
 
 class _RotatingImageContainerState extends State<RotatingImageContainer> {
   bool isHovered = false;
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
+
+    // Check if the screen width is less than a certain threshold (e.g., 600px for mobile)
+    bool isMobile = size.width < 600;
+
+    // Adjust image size based on whether it's mobile or not
+    double imageSize = isMobile ? size.width * 0.4 : size.width * 0.24;
+
     return MouseRegion(
       onEnter: (_) => setState(() => isHovered = true),
       onExit: (_) => setState(() => isHovered = false),
       child: AnimatedContainer(
-        duration: Duration(microseconds: 300),
-        transform: Matrix4.rotationZ(isHovered ? 0 : math.pi / 36),
-        height: size.width * 0.24,
-        width: size.width * 0.24,
+        duration: const Duration(milliseconds: 300), // Smooth animation
+        transform:
+            Matrix4.rotationZ(isHovered ? 0 : math.pi / 36), // Rotation effect
+        height: imageSize,
+        width: imageSize,
         decoration: BoxDecoration(
-            image: DecorationImage(
-                fit: BoxFit.cover,
-                image: AssetImage('assets/images/profile_new.jpg')),
-            border: Border.all(color: AppColors.studio, width: 1.2),
-            borderRadius: BorderRadius.circular(20)),
+          image: const DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage('assets/images/profile_new.jpg'),
+          ),
+          border: Border.all(color: AppColors.studio, width: 1.2),
+          borderRadius: BorderRadius.circular(20),
+        ),
       ),
     );
   }
