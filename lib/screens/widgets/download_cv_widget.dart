530f3483079c4a4e09b0e4cff9678d9c250867d0
Sufiyan <ksufi7350@gmail.com>
2024-10-11 20:40:42 +0500

    added links to buttons and icons

--- lib/screens/widgets/download_cv_widget.dart
+++ lib/screens/widgets/download_cv_widget.dart
@@ -1,38 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-
 import 'package:portfolio_web/constants/colors.dart';
+import 'package:url_launcher/url_launcher.dart'; // Import url_launcher
 
 class DownloadCVButton extends StatelessWidget {
-  const DownloadCVButton({
-    super.key,
-  });
+  const DownloadCVButton({super.key});
+
+  // URL of your CV on Google Drive
+  final String cvUrl =
+      'https://docs.google.com/document/d/1Jxth5gUbLlkiJ9f0b_Q2gUw7GYNSHYMA2WvSrCJU-bk/edit?usp=drive_link';
+
+  Future<void> _launchURL(String url) async {
+    final Uri uri = Uri.parse(url);
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(uri);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 50,
-      width: 250,
-      decoration: BoxDecoration(
+    return GestureDetector(
+      onTap: () => _launchURL(cvUrl), // Call the new method here
+      child: Container(
+        height: 50,
+        width: 250,
+        decoration: BoxDecoration(
           color: Colors.transparent,
           borderRadius: BorderRadius.circular(20),
-          border: Border.all(color: AppColors.paleSlate)),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            "Download CV",
-            style: TextStyle(color: AppColors.paleSlate),
-          ),
-          const SizedBox.square(
-            dimension: 12,
-          ),
-          FaIcon(
-            FontAwesomeIcons.download,
-            color: AppColors.paleSlate,
-            size: 18,
-          )
-        ],
+          border: Border.all(color: AppColors.paleSlate),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Download CV",
+              style: TextStyle(color: AppColors.paleSlate),
+            ),
+            const SizedBox.square(
+              dimension: 12,
+            ),
+            FaIcon(
+              FontAwesomeIcons.download,
+              color: AppColors.paleSlate,
+              size: 18,
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/screens/widgets/social_widget.dart
+++ lib/screens/widgets/social_widget.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:portfolio_web/constants/colors.dart';
 
 class SocialWidget extends StatelessWidget {
   const SocialWidget({super.key});
+
+  // Helper function to launch URLs
+  Future<void> _launchURL(String url) async {
+    final Uri uri = Uri.parse(url);
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(uri);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +32,8 @@ class SocialWidget extends StatelessWidget {
           child: Center(
             child: IconButton(
               hoverColor: AppColors.paleSlate,
-              onPressed: () {},
+              onPressed: () => _launchURL(
+                  'https://www.linkedin.com/in/muhammad-sufiyan-3179aa199/'),
               icon: FaIcon(
                 FontAwesomeIcons.linkedinIn,
                 color: AppColors.studio,
@@ -43,9 +55,10 @@ class SocialWidget extends StatelessWidget {
           child: Center(
             child: IconButton(
               hoverColor: AppColors.paleSlate,
-              onPressed: () {},
+              onPressed: () =>
+                  _launchURL('https://www.instagram.com/_just.sufi/'),
               icon: FaIcon(
-                FontAwesomeIcons.dribbble,
+                FontAwesomeIcons.instagram,
                 color: AppColors.studio,
                 size: 15,
               ),
@@ -65,9 +78,10 @@ class SocialWidget extends StatelessWidget {
           child: Center(
             child: IconButton(
               hoverColor: AppColors.paleSlate,
-              onPressed: () {},
+              onPressed: () => _launchURL(
+                  'https://www.facebook.com/mohammadsufiyan.irshad/'),
               icon: FaIcon(
-                FontAwesomeIcons.upwork,
+                FontAwesomeIcons.facebook,
                 color: AppColors.studio,
                 size: 15,
               ),
@@ -87,7 +101,7 @@ class SocialWidget extends StatelessWidget {
           child: Center(
             child: IconButton(
               hoverColor: AppColors.paleSlate,
-              onPressed: () {},
+              onPressed: () => _launchURL('https://github.com/31SUFI'),
               icon: FaIcon(
                 FontAwesomeIcons.github,
                 color: AppColors.studio,
