4edaa0e686f0aa38d48c2561f48c8761a3d1a5f3
Sufiyan <ksufi7350@gmail.com>
2024-10-15 20:00:55 +0500

    fix responsivess issue for techstackwidget

--- lib/screens/widgets/tech_stack_widget.dart
+++ lib/screens/widgets/tech_stack_widget.dart
@@ -1,124 +1,140 @@
 import 'package:flutter/material.dart';
 
 class TechStackSection extends StatelessWidget {
-  final Size size; // Add a parameter for size
+  final Size size;
 
-  const TechStackSection({super.key, required this.size}); // Constructor
+  TechStackSection({Key? key, required this.size}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    double screenWidth = MediaQuery.of(context).size.width;
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        bool isMobile = constraints.maxWidth < 600; // Define mobile size
 
-    // For responsiveness: mobile if width < 600, else desktop/tablet
-    bool isMobile = screenWidth < 600;
+        return Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: size.height * 0.02),
 
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        crossAxisAlignment:
-            CrossAxisAlignment.center, // Center align the column content
-        children: [
-          SizedBox(height: size.height * 0.02), // Responsive spacing
-
-          // Heading centered with RichText
-          RichText(
-            textAlign: TextAlign.center,
-            text: TextSpan(
-              text: 'I have attained ', // Normal text
-              style: TextStyle(
-                fontSize: isMobile ? 18 : 30, // Larger font for desktop
-                fontWeight: FontWeight.bold,
-                fontFamily: 'Poppins',
-                color: Colors.white,
+              // RichText for heading with dynamic font size
+              RichText(
+                textAlign: TextAlign.center,
+                text: TextSpan(
+                  text: 'I have attained ',
+                  style: TextStyle(
+                    fontSize: isMobile ? 18 : 30,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Poppins',
+                    color: Colors.white,
+                  ),
+                  children: [
+                    TextSpan(
+                      text: 'Expertise',
+                      style: TextStyle(
+                        fontSize: isMobile ? 18 : 30,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Poppins',
+                        foreground: Paint()
+                          ..strokeWidth = 1.5
+                          ..color = Colors.purpleAccent,
+                      ),
+                    ),
+                    const TextSpan(text: ' in an array of cutting-edge '),
+                    TextSpan(
+                      text: 'tech stacks',
+                      style: TextStyle(
+                        fontSize: isMobile ? 18 : 30,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Poppins',
+                        foreground: Paint()
+                          ..strokeWidth = 1.5
+                          ..color = Colors.purpleAccent,
+                      ),
+                    ),
+                  ],
+                ),
               ),
-              children: [
-                TextSpan(
-                  text: 'Expertise', // Highlighted and outlined word
-                  style: TextStyle(
-                    fontSize: isMobile ? 18 : 30,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'Poppins',
-                    foreground: Paint()
-                      ..strokeWidth = 1.5
-                      ..color = Colors.purpleAccent, // Outline color
-                  ),
+
+              const SizedBox(height: 16),
+
+              // Subheading
+              Text(
+                "Empowering me to craft seamless and innovative solutions.",
+                style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontSize: isMobile ? 16 : 20,
+                  color: Colors.purpleAccent,
                 ),
-                TextSpan(
-                  text: ' in an array of cutting-edge ', // Remaining text
+                textAlign: TextAlign.center,
+              ),
+
+              const SizedBox(height: 32),
+
+              // Description
+              Text(
+                "I have served as the Founder of growmonks, overseeing the entire project lifecycle "
+                "from ideation to successful client project delivery. As a Flutter Engineer at "
+                "Tickertape (10M+), I gained invaluable experience and deepened my understanding "
+                "of mobile development concepts.",
+                style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontStyle: FontStyle.italic,
+                  fontSize: isMobile ? 14 : 18,
+                  color: Colors.white,
                 ),
-                TextSpan(
-                  text: 'tech stacks', // Highlighted and outlined word
-                  style: TextStyle(
-                    fontSize: isMobile ? 18 : 30,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'Poppins',
-                    foreground: Paint()
-                      ..strokeWidth = 1.5
-                      ..color = Colors.purpleAccent, // Outline color
-                  ),
-                ),
-              ],
-            ),
+                textAlign: TextAlign.center,
+              ),
+
+              const SizedBox(height: 32),
+
+              // Tech Stack Icons with dynamic columns for responsiveness
+              LayoutBuilder(
+                builder: (context, constraints) {
+                  int crossAxisCount = isMobile
+                      ? 3
+                      : 6; // 3 columns on mobile, 6 on larger screens
+
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: GridView.builder(
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: crossAxisCount,
+                        crossAxisSpacing: 16,
+                        mainAxisSpacing: 16,
+                        childAspectRatio: 1, // Make each cell square
+                      ),
+                      itemCount: techStacks.length,
+                      itemBuilder: (context, index) {
+                        return _buildTechStackIcon(
+                          techStacks[index]['icon'] as IconData,
+                          techStacks[index]['label'] as String,
+                          isMobile,
+                        );
+                      },
+                    ),
+                  );
+                },
+              ),
+            ],
           ),
-
-          SizedBox(height: 16),
-
-          // Subheading centered
-          Text(
-            "Empowering me to craft seamless and innovative solutions.",
-            style: TextStyle(
-              fontFamily: 'Poppins',
-              fontSize: isMobile ? 18 : 24,
-              color: Colors.purpleAccent,
-            ),
-            textAlign: TextAlign.center, // Center align the text
-          ),
-
-          SizedBox(height: 32),
-
-          // Description centered
-          Text(
-            "I have served as the Founder of growmonks, overseeing the entire project lifecycle "
-            "from ideation to successful client project delivery. As a Flutter Engineer at "
-            "Tickertape (10M+), I gained invaluable experience and deepened my understanding "
-            "of mobile development concepts.",
-            style: TextStyle(
-              fontFamily: 'Poppins',
-              fontStyle: FontStyle.italic,
-              fontSize: isMobile ? 16 : 20,
-              color: Colors.white,
-            ),
-            textAlign: TextAlign.center, // Center align the text
-          ),
-
-          SizedBox(height: 32),
-
-          // Tech Stack Icons - use a GridView for responsiveness
-          Padding(
-            padding: const EdgeInsets.symmetric(
-                horizontal: 50.0), // Add padding around the grid
-            child: GridView.count(
-              shrinkWrap: true,
-              physics:
-                  NeverScrollableScrollPhysics(), // Prevent scrolling inside GridView
-              crossAxisCount: isMobile ? 3 : 6, // 3 for mobile, 6 for desktop
-              crossAxisSpacing: 16, // Adjusted spacing between columns
-              mainAxisSpacing:
-                  18, // Increased spacing between rows to avoid overlap
-              children: [
-                _buildTechStackIcon(Icons.flutter_dash, "Flutter", isMobile),
-                _buildTechStackIcon(Icons.storage, "Firebase", isMobile),
-                _buildTechStackIcon(Icons.code, "NodeJS", isMobile),
-                _buildTechStackIcon(Icons.functions_sharp, "Docker", isMobile),
-                _buildTechStackIcon(Icons.language, "Typescript", isMobile),
-                _buildTechStackIcon(Icons.cloud, "AWS", isMobile),
-              ],
-            ),
-          ),
-        ],
-      ),
+        );
+      },
     );
   }
+
+  // Tech stack data
+  final List<Map<String, dynamic>> techStacks = [
+    {'icon': Icons.flutter_dash, 'label': 'Flutter'},
+    {'icon': Icons.storage, 'label': 'Firebase'},
+    {'icon': Icons.code, 'label': 'NodeJS'},
+    {'icon': Icons.functions_sharp, 'label': 'Docker'},
+    {'icon': Icons.language, 'label': 'Typescript'},
+    {'icon': Icons.cloud, 'label': 'AWS'},
+  ];
 
   // Helper widget to create each icon with a label, adjusted for mobile
   Widget _buildTechStackIcon(IconData icon, String label, bool isMobile) {
@@ -127,19 +143,16 @@ class TechStackSection extends StatelessWidget {
       children: [
         CircleAvatar(
           backgroundColor: Colors.grey[800],
-          radius: isMobile ? 28 : 32, // Smaller circle for mobile
-          child: Icon(icon,
-              color: Colors.white,
-              size: isMobile ? 30 : 36), // Adjusted icon size
+          radius: isMobile ? 28 : 36, // Adjust radius based on screen size
+          child: Icon(icon, color: Colors.white, size: isMobile ? 30 : 40),
         ),
-        SizedBox(height: 13),
+        const SizedBox(height: 8),
         FittedBox(
-          // To ensure text fits within available space
           child: Text(
             label,
             style: TextStyle(
               color: Colors.white,
-              fontSize: isMobile ? 12 : 14, // Smaller text for mobile
+              fontSize: isMobile ? 12 : 14,
             ),
           ),
         ),
