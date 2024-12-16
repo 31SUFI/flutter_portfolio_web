45600a0c46d5c0a2e2e383d7159213ae61785e95
Muhammad Sufiyan <you@example.com>
2024-12-16 12:03:07 -0800

    minor changes commit

--- lib/Projects/data/model/projectsData.dart
+++ lib/Projects/data/model/projectsData.dart
@@ -3,16 +3,16 @@ import 'package:portfolio_web/Projects/data/model/projectmodel.dart';
 
 const List<Project> projects = [
   Project(
-    title: 'NayaPay Clone',
-    description: 'A complete clone of leading Fintech company ...',
-    githubUrl: 'https://github.com/31SUFI/NayaPay_Clone',
-    imageUrl: 'assets/images/nayapay.png',
-  ),
-  Project(
     title: 'Bookify',
     description: 'A user-friendly books library and management app...',
     githubUrl: 'https://github.com/31SUFI/Bookify',
     imageUrl: 'assets/images/bookify.png',
+  ),
+  Project(
+    title: 'NayaPay Clone',
+    description: 'A complete clone of leading Fintech company ...',
+    githubUrl: 'https://github.com/31SUFI/NayaPay_Clone',
+    imageUrl: 'assets/images/nayapay.png',
   ),
   Project(
     title: 'Project 03',
